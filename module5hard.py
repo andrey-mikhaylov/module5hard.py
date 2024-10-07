e79bc01b76afc79943a0120fdf619ce14c8611e4
Andrey <andrey.mikhaylov.n@gmail.com>
2024-10-07 11:39:34 +0300

    test

--- module5hard.py
+++ module5hard.py
@@ -1,11 +1,56 @@
+class UrTube:
+    pass
+
+class Video:
+    pass
+
+class User:
+    pass
+
 
 def main():
     pass
 
+def test():
+    ur = UrTube()
+    v1 = Video('Лучший язык программирования 2024 года', 200)
+    v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
-# Press the green button in the gutter to run the script.
+    # Добавление видео
+    ur.add(v1, v2)
+
+    # Проверка поиска
+    print(ur.get_videos('лучший'))
+    print(ur.get_videos('ПРОГ'))
+
+    # Проверка на вход пользователя и возрастное ограничение
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('vasya_pupkin', 'lolkekcheburek', 13)
+    ur.watch_video('Для чего девушкам парень программист?')
+    ur.register('urban_pythonist', 'iScX4vIJClb9YQavjAgF', 25)
+    ur.watch_video('Для чего девушкам парень программист?')
+
+    # Проверка входа в другой аккаунт
+    ur.register('vasya_pupkin', 'F8098FM8fjm9jmi', 55)
+    print(ur.current_user)
+
+    # Попытка воспроизведения несуществующего видео
+    ur.watch_video('Лучший язык программирования 2024 года!')
+"""
+Вывод в консоль:
+['Лучший язык программирования 2024 года']
+['Лучший язык программирования 2024 года', 'Для чего девушкам парень программист?']
+Войдите в аккаунт, чтобы смотреть видео
+Вам нет 18 лет, пожалуйста покиньте страницу
+1 2 3 4 5 6 7 8 9 10 Конец видео
+Пользователь vasya_pupkin уже существует
+urban_pythonist
+"""
+
+
 if __name__ == '__main__':
-    main()
+    test()
+    #main()
 
 
 """
